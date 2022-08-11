aca00d6e65bfc57d0ab8941c7e3571c60e4259f6
FilipeMartins <filipe@bling.eu>
2022-08-11 17:59:41 +0200

    chore: remove comments

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,11 +9,3 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
-    
-    
-# def test_create_user(client):
-#     data = {"username":"testuser","email":"testuser@nofoobar.com","password":"testing"}
-#     response = client.post("/users/",json.dumps(data))
-#     assert response.status_code == 200 
-#     assert response.json()["email"] == "testuser@nofoobar.com"
-#     assert response.json()["is_active"] == True
